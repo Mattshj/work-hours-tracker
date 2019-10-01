f12d0182297fa950525c4939e261884f39ecf758
mehrdad <mehrdadjaberi1375@gmail.com>
2019-10-01 13:33:48 +0330

    complete project untill section 4

---
+++ polls/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Job, JobBox
+
+admin.site.register(Job)
+admin.site.register(JobBox)
+
+# Register your models here.
---
+++ polls/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from polls.models import Job, JobBox
+
+
+def home(request):
+    jobs = Job.objects.all()
+    JobsBox = JobBox.objects.all()
+    args = {
+        'jobs': jobs,
+        'jobsbox': JobsBox
+    }
+    return render(request, 'home.html', args)
+
+
+def login(request):
+    return HttpResponse("Login")
